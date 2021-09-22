d9be09444c5f8b3f72db916bfd4e5a912fa759a2
Recons7 <81979170+Recons7@users.noreply.github.com>
2021-09-22 13:14:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -64,3 +64,15 @@ function init() {
   // Init TypeWriter
   new TypeWriter(txtElement, words, wait);
 }
+
+var cursor = true;
+        var speed = 250;
+        setInterval(() => {
+          if(cursor) {
+            document.getElementById('cursor').style.opacity = 0;
+            cursor = false;
+          }else {
+            document.getElementById('cursor').style.opacity = 1;
+            cursor = true;
+          }
+        }, speed);
