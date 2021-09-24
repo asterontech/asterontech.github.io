c96f17aefd6f0fab8a822a8a788ecf2607f99cfd
Recons7 <81979170+Recons7@users.noreply.github.com>
2021-09-24 18:17:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -66,13 +66,13 @@ function init() {
 }
 
 var cursor = true;
-        var speed = 250;
-        setInterval(() => {
-          if(cursor) {
-            document.getElementById('cursor').style.opacity = 0;
-            cursor = false;
-          }else {
-            document.getElementById('cursor').style.opacity = 1;
-            cursor = true;
-          }
-        }, speed);
+var speed = 250;
+setInterval(() => {
+  if (cursor) {
+    document.getElementById('cursor').style.opacity = 0;
+    cursor = false;
+  } else {
+    document.getElementById('cursor').style.opacity = 1;
+    cursor = true;
+  }
+}, speed);
