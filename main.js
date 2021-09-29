9dffe8b4a52758f1aea12f22555196191fe62a8e
asterontech <91524574+asterontech@users.noreply.github.com>
2021-09-29 13:33:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -65,6 +65,30 @@ function init() {
   new TypeWriter(txtElement, words, wait);
 }
 
+
+(function() {
+  const buttons = document.querySelectorAll(".btn-posnawr");
+
+  buttons.forEach(button => {
+    ["mouseenter", "mouseout"].forEach(evt => {
+      button.addEventListener(evt, e => {
+        let parentOffset = button.getBoundingClientRect(),
+            relX = e.pageX - parentOffset.left,
+            relY = e.pageY - parentOffset.top;
+
+        const span = button.getElementsByTagName("span");
+
+        span[0].style.top = relY + "px";
+        span[0].style.left = relX + "px";
+      });
+    });
+  });
+})();
+
+
+
+
+
 var cursor = true;
 var speed = 300;
 setInterval(() => {
