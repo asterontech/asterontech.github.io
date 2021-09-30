63a5d8ff0d64759857134b4914d533b41be4ae46
asterontech <91524574+asterontech@users.noreply.github.com>
2021-09-30 13:53:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -69,13 +69,13 @@ function init() {
 $(function() {  
   $('.sbtn-posnawr')
     .on('mouseenter', function(e) {
-			var parentOffset = $(this).offset(),
+	var parentOffset = $(this).offset(),
       		relX = e.pageX - parentOffset.left,
       		relY = e.pageY - parentOffset.top;
-			$(this).find('span').css({top:relY, left:relX})
+	$(this).find('span').css({top:relY, left:relX})
     })
     .on('mouseout', function(e) {
-			var parentOffset = $(this).offset(),
+	var parentOffset = $(this).offset(),
       		relX = e.pageX - parentOffset.left,
       		relY = e.pageY - parentOffset.top;
     	$(this).find('span').css({top:relY, left:relX})
